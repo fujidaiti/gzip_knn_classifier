47acd30f8976d9d3780f5881b2a8fca596fc445b
fujidaiti <fujidaiti@gmail.com>
2023-08-02 20:36:52 +0900

    init

---
+++ package/example/knn_gzip_classifier_example.dart
@@ -0,0 +1,6 @@
+import 'package:knn_gzip_classifier/knn_gzip_classifier.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ package/test/knn_gzip_classifier_test.dart
@@ -0,0 +1,16 @@
+import 'package:knn_gzip_classifier/knn_gzip_classifier.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
